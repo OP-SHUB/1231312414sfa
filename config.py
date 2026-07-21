7a681de5624c986edaa3b43a092a3610be916e45
OP-SHUB <125016943+OP-SHUB@users.noreply.github.com>
2026-07-22 02:52:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8882393557:AAGLyUD-P10dhN-Wd4MQrWZN99aG2tSSb-c")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8276709487:AAEYesKZxhj7zBLxKi16UqUFJkqlRn7VB9o")
 ADMIN_IDS = [
     int(x) for x in os.environ.get("ADMIN_IDS", "7410975556").split(",") if x.strip()
 ]
 KEYS_FILE = os.environ.get("KEYS_FILE", "keys.txt")
 USERS_FILE = os.environ.get("USERS_FILE", "users.txt")
+DEVICES_FILE = os.environ.get("DEVICES_FILE", "devices.txt")
